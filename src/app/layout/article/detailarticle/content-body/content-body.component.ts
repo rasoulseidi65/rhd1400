8659c38d49a4d33d5489aa9d8ae38ade0d27e198
hd724 <asp.net1367@gmail.com>
2021-04-19 00:49:07 +0430

    30/1/1400

--- src/app/layout/article/detailarticle/content-body/content-body.component.ts
+++ src/app/layout/article/detailarticle/content-body/content-body.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {LayoutService} from '../../../layout.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'article-content-body',
@@ -7,9 +10,20 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class ContentBodyComponent implements OnInit {
   @Input() data: any;
-  constructor() { }
+  articleID: any;
+  article: any;
+  constructor(private route: ActivatedRoute,private service: LayoutService) { }
 
   ngOnInit(): void {
+    this.loadArtile();
   }
-
+  loadArtile() {
+    this.route.paramMap.subscribe(params =>
+      this.articleID = params.get('id'));
+    this.service.findArticle(this.articleID).subscribe((response) => {
+      if (response['success'] === true) {
+        this.article = response['data'];
+      }
+    });
+  }
 }

e0e90178c34da47f9f9b6353914c43bc0753bfab
hd724 <asp.net1367@gmail.com>
2021-04-22 15:47:48 +0430

    news

--- src/app/Core/LayoutAdmin/Courses/listcourses/listcourses.component.ts
+++ src/app/Core/LayoutAdmin/Courses/listcourses/listcourses.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listcourses.component.scss']
 })
 export class ListcoursesComponent implements OnInit {
-
+  cols: any[];
   constructor() { }
 
   ngOnInit() {
+    this.cols = [
+      {field: 'نام مدرس', header: 'نام مدرس'},
+      {field: 'عنوان دوره', header: 'عنوان دوره'},
+      {field: 'تاریخ تولید', header: 'تاریخ تولید'},
+      {field: 'قیمت', header: 'قیمت'},
+      {field: 'عملیات', header: 'عملیات'}
+    ];
   }
 
 }

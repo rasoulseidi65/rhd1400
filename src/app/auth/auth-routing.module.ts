56d9666a426e6801445253ad06f12e134e33ea33
hd724 <asp.net1367@gmail.com>
2021-04-27 20:14:57 +0430

    new

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
+import {LoginAdminComponent} from './login-admin/login-admin.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: 'login-admin',
+    component: LoginAdminComponent
   },
 ];
 

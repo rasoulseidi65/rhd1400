b15ecc6855cc6c710ea98df740ed9a244e642edd
hd724 <asp.net1367@gmail.com>
2021-05-08 23:44:10 +0430

    .

---
+++ src/app/layout/worksamples/worksamples.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-worksamples',
+  templateUrl: './worksamples.component.html',
+  styleUrls: ['./worksamples.component.scss']
+})
+export class WorksamplesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
